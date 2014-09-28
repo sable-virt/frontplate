7f06fa82077a2771ead9c15ec964544b41bdab0f
frontainer <frontainer@gmail.com>
2014-09-29 07:35:00 +0900

    WebPackの設定ファイルを追加

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+var webpack = require("webpack");
+var config = require('./frontplate');
+module.exports = {
+    output: {
+        filename: "[name].js",
+        sourceMapFilename: 'map/[file].map'
+    },
+    devtool: config.debug ? '#eval' : '#source-map',
+    resolve: {
+        modulesDirectories: ["bower_components","node_modules"],
+        alias: {
+            bower: 'bower_components'
+        }
+    },
+    module: {
+        preLoaders: [
+            {
+                loader: "source-map-loader"
+            }
+        ],
+        loaders: [
+            { test: /\.html$/, loader: 'html-loader' }
+        ]
+    },
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin('common','common.js'),
+        new webpack.ResolverPlugin(
+            new webpack.ResolverPlugin.DirectoryDescriptionFilePlugin("bower.json", ["main"])
+        ),
+        new webpack.optimize.DedupePlugin()
+    ]
+};
