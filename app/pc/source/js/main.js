d90a0328177dd16f46ccbbe9bfab2f68975693f9
frontainer <frontainer@gmail.com>
2014-12-26 23:10:19 +0900

    ディレクトリ変更により削除

--- app/pc/source/js/main.js
+++
@@ -1,8 +0,0 @@
-(function () {
-    "use strict";
-    require('angular');
-    angular.module('app',[]).controller('RootCtrl', function($scope) {
-        console.log('root controller');
-        $scope.hoge = '555';
-    });
-})();
