1799c4b8dadac2dae6694a72a30f7bfeb1bc8eff
frontainer <frontainer@gmail.com>
2014-12-26 23:09:35 +0900

    タスクの整理

--- gulp/ejs.js
+++ gulp/ejs.js
@@ -3,18 +3,11 @@ var gulp = require('gulp'),
     $ = frontplate.plugins;
 
 module.exports = function () {
-    gulp.task('ejs',function() {
+    gulp.task('ejs', function () {
         return gulp.src(frontplate.getPath('ejs'))
-            .pipe($.plumber({
-                errorHandler: function(e) {
-                    this.unpipe();
-                }
-            }))
-            .pipe($.ejs(config.ejs))
-            .on('error', $.notify.onError("<%= error.message %>"))
+            .pipe($.plumber({errorHandler: $.notify.onError('<%= error.message %>')}))
+            .pipe($.ejs(frontplate.config.ejs))
             .pipe(gulp.dest(frontplate.getPath('ejs','dest')))
             .pipe($.browser.reload({stream: true}));
     });
 }();
-
-
