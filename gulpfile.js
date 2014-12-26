36dee0fe647e002affbbd3094178a3af9dc441d5
frontainer <frontainer@gmail.com>
2014-12-26 23:09:27 +0900

    ディレクトリを自由に設定できるようにオプション設定できるように

--- gulpfile.js
+++ gulpfile.js
@@ -5,36 +5,29 @@ var gulp = require('gulp'),
         replaceString: /\bgulp[\-.]/
     });
 
+var runSequence = require('run-sequence');
+var extend = require('extend');
 $.browser = require('browser-sync');
 
-var DIRECTORIES = ['pc','sp','admin'];
-var BASE_TYPE = getBaseType();
-var BASE_PATH = getBasePath();
-var MINIFY = process.argv.indexOf('-min') === -1 ? false : true;
-
-function getBasePath() {
-    var path = config.APP_PATH;
-    if (!config.flatten) {
-        path += '/' + BASE_TYPE;
-    }
-    return path;
-}
-
-function getBaseType() {
-    var type = '';
-    if (!config.flatten) {
-        for (var i = 0, len = DIRECTORIES.length; i < len; i++) {
-            if (process.argv.indexOf('-' + DIRECTORIES[i]) !== -1) {
-                type = DIRECTORIES[i];
-                break;
-            }
-        }
-        if (!type) {
-            type = DIRECTORIES[0];
+var DEFAULT_OPTIONS = {
+    min: false,
+    d: config.defaultDir || ''
+};
+var APP_PATH = config.appPath;
+var OPTIONS = parseOption(process.argv);
+function parseOption(argv) {
+    var options = extend({},DEFAULT_OPTIONS);
+    for(var i = 0, len = argv.length; i < len; i++) {
+        if (argv[i].charAt(0) === '-') {
+            var match = argv[i].substring(1).split('=');
+            var key = match[0];
+            var value = match[1] || true;
+            options[key] = value;
         }
     }
-    return type;
+    return options;
 }
+
 
 /**
  * パス生成関数
@@ -42,20 +35,29 @@ function getBaseType() {
  * @param prop
  * @returns {string}
  */
-function getPath(name,prop) {
+function getPath(name, prop) {
     prop = prop || 'src';
+    if (!config.path[name] || !config.path[name][prop]) {
+        return console.log('[ERROR] not found path - ' + name + '.' + prop);
+    }
     var path = config.path[name][prop];
     if (typeof path === 'object') {
-        return path.map(function(val) {
+        return path.map(function (val) {
             if (val.charAt(0) === '!') {
-                return '!' + BASE_PATH + '/' + val.substring(1);
+                return '!' + APP_PATH + '/' + replaceTypeTag(val.substring(1));
             }
-            return BASE_PATH + '/' + val;
-        })
+            return APP_PATH + '/' + replaceTypeTag(val);
+        });
     } else {
         path = path || '';
-        return BASE_PATH + '/' + path;
+        return APP_PATH + '/' + replaceTypeTag(path);
     }
+}
+function replaceTypeTag(val) {
+    if (OPTIONS.d) {
+        return val.replace(/%type%/g, OPTIONS.d);
+    }
+    return val.replace(/\/%type%/g, '');
 }
 
 // globalに共通変数書き出し
@@ -63,22 +65,19 @@ global.frontplate = {
     plugins: $,
     config: config,
     getPath: getPath,
-    BASE_TYPE: BASE_TYPE,
-    BASE_PATH: BASE_PATH,
-    MINIFY: MINIFY
+    option: OPTIONS
 };
 
 gulp.task('watch', function() {
-    gulp.watch(getPath('js'), ['jshint']);
     gulp.watch(getPath('ejs','watch'), ['ejs']);
-    gulp.watch(getPath('iconfont'), ['iconfont','style']);
     gulp.watch(getPath('sass'), ['style']);
-    gulp.watch(getPath('html'), ['htmllint']);
     gulp.watch(getPath('sprite','watch'), ['sprite']);
     gulp.watch(getPath('images'), ['imagemin']);
 });
 
 // gulpディレクトリのタスク読み込み
 var tasks = require('./gulp/load');
-gulp.task('build',['ejs','iconfont','style','sprite','imagemin'], function() {});
+gulp.task('build',['clean'], function(callback) {
+    return runSequence(['ejs', 'style', 'imagemin', 'script'], callback);
+});
 gulp.task('default',['server','watch','watchScript'], function() {});
