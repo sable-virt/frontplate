10f2aeb08d88b1563f3b99d0c6ca1034b6626440
frontainer <frontainer@gmail.com>
2015-03-29 18:18:50 +0900

    複数ディレクトリ持てるよう修正

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,4 @@
+
 var gulp = require('gulp'),
     config = require('./config');
     $ = require('gulp-load-plugins')({
@@ -8,16 +9,42 @@ var gulp = require('gulp'),
 var runSequence = require('run-sequence');
 $.browser = require('browser-sync');
 
+var DIR = getType(process.argv);
+
+function getType(args) {
+  var result = {};
+  for (var i = 0, len = args.length; i < len; i++) {
+    if (args[i].charAt(0) === '-') {
+      return args[i].replace(/^-/,'');
+    }
+  }
+  return config.defaultPath;
+}
+
 // globalに共通変数書き出し
 global.$ = $;
 global.config = config;
 
+function buildPath(data) {
+  for (var key in data) {
+    var val = data[key];
+    if (typeof val === 'string') {
+      data[key] = val.replace(/%type%/g,DIR);
+    } else {
+      data[key] = buildPath(val);
+    }
+  }
+  return data;
+}
+
+buildPath(config.path);
+
 gulp.task('watch', function() {
-    gulp.watch(getPath('ejs','watch'), ['ejs']);
-    gulp.watch(getPath('html'), ['html']);
-    gulp.watch(getPath('sass'), ['style']);
-    gulp.watch(getPath('sprite','watch'), ['sprite','style','copy']);
-    gulp.watch(getPath('images'), ['copy']);
+    gulp.watch(config.path.ejs.watch, ['ejs']);
+    gulp.watch(config.path.html.src, ['html']);
+    gulp.watch(config.path.style.src, ['style']);
+    gulp.watch(config.path.sprite.watch, ['sprite','style','copy']);
+    gulp.watch(config.path.images, ['copy']);
 });
 
 // gulpディレクトリのタスク読み込み
