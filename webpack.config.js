10b68741b7c4492e37642dbcbd407f86099b6ee2
frontainer <frontainer@gmail.com>
2015-03-31 10:20:36 +0900

    webpackプラグインを諸々追加

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,20 @@ module.exports = {
         ]
     },
     plugins: [
+        new webpack.DefinePlugin({
+            __IS_PRODUCTION: JSON.stringify(global.isProduction)
+        }),
+        new webpack.ResolverPlugin(
+            new webpack.ResolverPlugin.DirectoryDescriptionFilePlugin("bower.json", ["main"])
+        ),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        }),
         new webpack.optimize.CommonsChunkPlugin('app','app.js'),
-        new webpack.optimize.AggressiveMergingPlugin()
+        new webpack.optimize.AggressiveMergingPlugin(),
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.optimize.DedupePlugin()
     ]
 };
