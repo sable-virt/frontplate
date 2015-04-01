8cb1a7de111651f9dac21142ef32a1520b1a24e4
frontainer <frontainer@gmail.com>
2015-04-01 10:02:00 +0900

    karma-mocha-reporterでターミナルが埋まるのでprogressに

--- karma.conf.js
+++ karma.conf.js
@@ -21,7 +21,7 @@ module.exports = function(config) {
         // test results reporter to use
         // possible values: 'dots', 'progress'
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-        reporters: ['mocha'],
+        reporters: ['progress'],
         coverageReporter: {
             type: 'html',
             dir: 'coverage/'
