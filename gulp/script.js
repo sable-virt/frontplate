13c1b7940016f98bde095ee6c1eeabad7f061cd2
frontainer <frontainer@gmail.com>
2015-04-08 17:47:17 +0900

    js圧縮はprodのときのみ実行するよう修正。tsでもjsでもwebpackで処理できるように修正

--- gulp/script.js
+++ gulp/script.js
@@ -1,18 +1,25 @@
 var gulp = require('gulp'),
     through = require('through2'),
-    path = require('path');
+    path = require('path'),
+    webpack = require('webpack');
 
-var webpack = require('webpack');
 function exeWebPack(watch) {
     var conf = Object.create(require('../webpack.config.js'));
     conf.watch = watch;
     if(global.__IS_PRODUCTION) {
         delete conf.devtool;
+        conf.plugins = conf.plugins || [];
+        conf.plugins.push(new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: true
+            }
+        }));
     }
     return gulp.src(__CONFIG.path.js.src)
         .pipe(through.obj(function(file,charset,callback) {
             conf.entry = conf.entry || {};
-            conf.entry[path.basename(file.path,'.ts')] = file.path;
+            var fileName = path.basename(file.path).replace(/\.(ts|js)$/,'');
+            conf.entry[fileName] = file.path;
             this.push(file);
             callback();
         }))
