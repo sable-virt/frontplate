1a2b1fd4748775827ee71b9e549d71e50f39bf0d
frontainer <frontainer@gmail.com>
2015-04-18 10:55:13 +0900

    BrowserSyncの設定をconfig.jsから設定できるように

--- gulp/server.js
+++ gulp/server.js
@@ -1,9 +1,10 @@
 var gulp = require('gulp');
+var _ = require('lodash');
 var rewrite = require('connect-modrewrite');
 
 module.exports = function () {
     gulp.task('server',function() {
-        return $.browser({
+        var options = _.merge(__CONFIG.server,{
             server: {
                 baseDir: __CONFIG.dist,
                 directory: false,
@@ -13,14 +14,9 @@ module.exports = function () {
                     ])
                 ]
             },
-            notify: false,
-            ghostMode: {
-                clicks: false,
-                location: false,
-                forms: false,
-                scroll: false
-            }
+            notify: false
         });
+        return $.browser(options);
     });
     gulp.task('reload',function() {
         $.browser.reload();
