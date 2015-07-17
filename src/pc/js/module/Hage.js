5e64d7fda6b5a8b5b27b95cdddd8381c66b4e44b
frontainer <frontainer@gmail.com>
2015-07-17 19:19:08 +0900

    chore(etc): デモとして置いていたファイルを削除

--- src/pc/js/module/Hage.js
+++
@@ -1,7 +0,0 @@
-import Hoge from './Hoge';
-class Hage extends Hoge {
-    constructor() {
-        super('hage');
-    }
-}
-export default Hage;
--- src/pc/test/hogeSpec.js
+++
@@ -1,8 +0,0 @@
-describe('hoge', () => {
-    beforeEach(() => {
-
-    });
-    it('init', () => {
-
-    });
-});
