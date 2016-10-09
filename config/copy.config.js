136b8ddd98aa8e4af466c17b5e60812892a698c1
frontainer <frontainer@gmail.com>
2016-10-09 12:23:53 +0900

    設定ファイルは一旦隠蔽する

--- config/copy.config.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-const core = require('./core.config');
-module.exports = {
-    // 'path/from': '/path/to'
-    'src/lib/**/*': core.basePath + '/assets/lib'
-};
--- config/image.config.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-const core = require('./core.config');
-module.exports = {
-    src: 'src/images/*.{gif,jpg,png}',  // 読み込むイメージ
-    dest: core.basePath + '/assets/images'        // 出力先
-};
