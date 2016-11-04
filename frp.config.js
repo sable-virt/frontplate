4b54a10c64247b96f05cb274eb52ecfb6f40c02b
frontainer <frontainer@gmail.com>
2016-11-04 11:29:09 +0900

    update frp.config.js v2.0.0

--- frp.config.js
+++ frp.config.js
@@ -1,7 +1,8 @@
 'use strict';
 // https://github.com/frontainer/frontplate-cli/wiki/6.%E8%A8%AD%E5%AE%9A
 module.exports = function(production) {
-  global.FRP_DEST = global.FRP_DEST || 'public';
+  global.FRP_SRC = 'src';
+  global.FRP_DEST = 'public';
   return {
     clean: {},
     html: {},
