c2f7623b59ad13509d540f5cfdbe8bd76b0f6819
y_hokkey <hokkey@users.noreply.github.com>
2017-10-25 10:53:41 +0900

    元の設定ファイルへのURLを追記

--- frp.config.js
+++ frp.config.js
@@ -1,5 +1,6 @@
 'use strict';
 // https://github.com/frontainer/frontplate-cli/wiki/6.%E8%A8%AD%E5%AE%9A
+// https://github.com/frontainer/frontplate-cli/tree/master/config
 module.exports = function(production) {
   global.FRP_SRC = 'src';
   global.FRP_DEST = 'public';
